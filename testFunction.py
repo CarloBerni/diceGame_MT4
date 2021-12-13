adde957861506e208f2961f04916bb05c403806c
Nicolas <n_triau@hetic.eu>
2021-12-13 12:07:54 +0100

    Init function

---
+++ testFunction.py
@@ -0,0 +1,27 @@
+# Target total score to win by default
+DEFAULT_TARGET_SCORE = 2000
+
+# Number of dices by default in the set
+DEFAULT_DICES_NB = 5
+# Number of side of the dices used in the game
+NB_DICE_SIDE = 6
+
+# List of dice value scoring
+LIST_SCORING_DICE_VALUE = [1, 5]
+# List of associated score for scoring dice values
+LIST_SCORING_MULTIPLIER = [100, 50]
+
+# Trigger for multiple bonus
+TRIGGER_OCCURRENCE_FOR_BONUS = 3
+# Special bonus multiplier for multiple ace bonus
+BONUS_VALUE_FOR_ACE_BONUS = 1000
+# Standard multiplier for multiple dices value bonus
+BONUS_VALUE_FOR_NORMAL_BONUS = 100
+
+import random
+
+def calculate_iterations (threw_dices):
+  array_occuration = [0] * NB_DICE_SIDE
+  for index in range(threw_dices):
+    random_number = random.randint(1, NB_DICE_SIDE)
+    array_occuration[random_number - 1] += 1

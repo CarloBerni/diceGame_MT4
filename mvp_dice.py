a63053d44ca568d3a6628754007c060c5dd3bc84
Nicolas <n_triau@hetic.eu>
2021-12-13 16:05:58 +0100

    feat: calculate points functions

--- mvp_dice.py
+++ mvp_dice.py
@@ -26,4 +26,31 @@ def roll_dice_set(nb_dice_to_roll):
     for i in range(nb_dice_to_roll):
         random_number = randint(1, NB_DICE_SIDE)
         occurences[random_number - 1] += 1
+
     return occurences
+
+
+def bonus_point(occArray):
+    bonus = 0
+    for index, occ in enumerate(occArray):
+        if index != 0 and occ >= TRIGGER_OCCURRENCE_FOR_BONUS:
+            bonus += (index + 1) * BONUS_VALUE_FOR_NORMAL_BONUS
+            occArray[index] -= TRIGGER_OCCURRENCE_FOR_BONUS
+        elif index == 0 and occ >= TRIGGER_OCCURRENCE_FOR_BONUS:
+            bonus += BONUS_VALUE_FOR_ACE_BONUS
+            occArray[index] -= TRIGGER_OCCURRENCE_FOR_BONUS
+
+    return occArray, bonus
+
+
+def total_score(nb_roll):
+    array_occ = roll_dice_set(nb_roll)
+    new_array, score = bonus_point(array_occ)
+    for index, occ in enumerate(new_array):
+        if index + 1 in LIST_SCORING_DICE_VALUE:
+            score += occ * LIST_SCORING_MULTIPLIER[LIST_SCORING_DICE_VALUE.index(index + 1)]
+
+    return score
+
+
+print(total_score(8))

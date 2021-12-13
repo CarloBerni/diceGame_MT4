bb5fb4ec355987cef5f881c974cba1dcbc04bec8
Louis Genestier <louisgenestier@MacBook-Pro-de-Louis.local>
2021-12-13 15:04:31 +0100

    feat: basic features

---
+++ main.py
@@ -0,0 +1,50 @@
+import random
+
+NB_DICE_SIDE = 6  # Nb of side of the Dices
+SCORING_DICE_VALUE_LIST = [1, 5]  # List of the side values of the dice who trigger a standard score
+SCORING_MULTIPLIER_LIST = [100, 50]  # List of multiplier for standard score
+
+THRESHOLD_BONUS = 3  # Threshold of the triggering for bonus in term of occurrence of the same slide value
+STD_BONUS_MULTIPLIER = 100  # Standard multiplier for bonus
+ACE_BONUS_MULTIPLIER = 1000  # Special multiplier for aces bonus
+
+
+# return a list of dices value occurrence for a roll of nb_dice_to_roll dices
+def roll_dice_set(nb_dice_to_roll):
+    dice_value_occurrence_list = [0] * NB_DICE_SIDE
+    for n in range(nb_dice_to_roll):
+        dice_value = random.randint(1, NB_DICE_SIDE)
+        dice_value_occurrence_list[dice_value - 1] += 1
+
+    return dice_value_occurrence_list
+
+
+def analyse_bonus_score(dice_value_occurrence_list):
+    score = 0
+    for side_value_index, dice_value_occurrence in enumerate(dice_value_occurrence_list):
+        nb_of_bonus = dice_value_occurrence // THRESHOLD_BONUS
+        if nb_of_bonus > 0:
+            if side_value_index == 0:
+                bonus_multiplier = ACE_BONUS_MULTIPLIER
+            else:
+                bonus_multiplier = STD_BONUS_MULTIPLIER
+            score += nb_of_bonus * bonus_multiplier * (side_value_index + 1)
+            dice_value_occurrence_list[side_value_index] %= THRESHOLD_BONUS
+
+    return score, dice_value_occurrence_list
+
+
+def analyse_standard_score(dice_value_occurrence_list):
+    score = 0
+    for scoring_value, scoring_multiplier in zip(SCORING_DICE_VALUE_LIST, SCORING_MULTIPLIER_LIST):
+        score += dice_value_occurrence_list[scoring_value - 1] * scoring_multiplier
+        dice_value_occurrence_list[scoring_value - 1] = 0
+
+    return score, dice_value_occurrence_list
+
+
+def analyse_score(dice_value_occurrence_list):
+    bonus_score, dice_value_occurrence_list = analyse_bonus_score(dice_value_occurrence_list)
+    standard_score, dice_value_occurrence_list = analyse_standard_score(dice_value_occurrence_list)
+
+    return bonus_score + standard_score,

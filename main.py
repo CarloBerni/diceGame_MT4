66f93fbb12f735d2e898d7022a7062b52b01af7f
Louis Genestier <louisgenestier@MacBook-Pro-de-Louis.local>
2021-12-17 11:42:24 +0100

    feat: refacto

--- main.py
+++ main.py
@@ -73,6 +73,30 @@ def init_scoreboard():
         }
     return scoreboard
 
+def init_stats():
+    return {
+        "scored_max_turn": {
+            "player": "",
+            "value": 0
+        },
+        "max_loss_turn": {
+            "player": "",
+            "value": 0
+        },
+        "longest_turn": {
+            "player": "",
+            "value": 0
+        },
+        "scoring_turn": {
+            "turns": 1,
+            "score": 0
+        },
+        "non_scoring_turn": {
+            "turns": 1,
+            "score": 0
+        }
+    }
+
 
 def get_sorted_scoreboard(scoreboard):
     sorted_dict_keys = sorted(scoreboard, key=lambda x: (scoreboard[x]['score']), reverse=True)
@@ -89,16 +113,34 @@ def print_total_score(scoreboard):
         total_score += f"{player}--> {current_player_score}, "
     print(total_score + "\n")
 
+def get_formatted_winning_occurences(occurences):
+    formatted_winning_occurences = []
+    for indexOcc, occValue in enumerate(occurences):
+        if occValue >= THRESHOLD_BONUS:
+            formatted_winning_occurences.append([occValue, indexOcc + 1])
+
+        elif indexOcc + 1 in SCORING_DICE_VALUE_LIST and occValue > 0:
+            formatted_winning_occurences.append([occValue, indexOcc + 1])
+        
+
+    return formatted_winning_occurences  
+
+
+def print_endgame_stats(stats):
+    print("\n")
+    print(f"Max turn scoring : {stats['scored_max_turn']['player']} with {stats['scored_max_turn']['value']}")
+    print(f"Longest turn : {stats['longest_turn']['player']} with {stats['longest_turn']['value']}")
+    print(f"Max turn loss : {stats['max_loss_turn']['player']} with {stats['max_loss_turn']['value']}")
+    print("\n")
+    print(f"Mean scoring turn : {round(stats['scoring_turn']['score'] / stats['scoring_turn']['turns'], 2)} ({stats['scoring_turn']['turns']} turns)")
+    print(f"Mean non scoring turn : {round(stats['non_scoring_turn']['score'] / stats['non_scoring_turn']['turns'], 2)} ({stats['non_scoring_turn']['turns']} turns)")          
 
 def game():
     is_finished = False
     current_turn = 0
     scoreboard = init_scoreboard()
-    scored_max_turn = ['', 0]
-    max_loss_turn = ['', 0]
-    longest_turn = ['', 0]
-    scoring_turn = [0, 0]
-    non_scoring_turn = [0, 0]
+    stats = init_stats()
+
     while not is_finished:
         current_turn += 1
         for player in PLAYERS:
@@ -121,12 +163,13 @@ def game():
                 current_roll += 1
                 scoreboard[player]['rolls'] += 1
                 dices_occurences = roll_dice_set(remaining_dices)
+                formatted_winning_occurences = get_formatted_winning_occurences(dices_occurences)
                 previous_remaining_dices = remaining_dices
                 potential_roll_score, times_bonus = analyse_score(dices_occurences)
                 remaining_dices = analyse_dices_to_roll(remaining_dices, dices_occurences)
                 potential_turn_score += potential_roll_score
                 
-                print(f"roll #{current_roll} : {previous_remaining_dices - remaining_dices} scoring dices scoring {potential_roll_score}, potential total turn score {potential_turn_score}, remaining dice to roll : {remaining_dices}")
+                print(f"roll #{current_roll} : {previous_remaining_dices - remaining_dices} scoring dices {str(formatted_winning_occurences)} scoring {potential_roll_score}, potential total turn score {potential_turn_score}, remaining dice to roll : {remaining_dices}")
                 
 
                 # Stats
@@ -134,9 +177,9 @@ def game():
                     scoreboard[player]['full_roll'] += 1
                     remaining_dices = DEFAULT_DICES_NB
 
-                if current_roll > longest_turn[1]:
-                    longest_turn[0] = player
-                    longest_turn[1] = current_roll
+                if current_roll > stats["longest_turn"]["value"]:
+                    stats["longest_turn"]["player"] = player
+                    stats["longest_turn"]["value"] = current_roll
 
                 scoreboard[player]['bonus'] += times_bonus
 
@@ -150,25 +193,24 @@ def game():
             if is_looser:
                 print(f"you lose this turn and a potential to score {potential_turn_score} pts\n")
                 scoreboard[player]["lost_score"] += potential_turn_score
-                non_scoring_turn[0] += 1
-                non_scoring_turn[1] += potential_turn_score
+                stats["non_scoring_turn"]["turns"] += 1
+                stats["non_scoring_turn"]["score"] += potential_turn_score
 
-                if potential_turn_score > max_loss_turn[1]:
-                    max_loss_turn[0] = player
-                    max_loss_turn[1] = potential_turn_score
+                if potential_turn_score > stats["max_loss_turn"]["value"]:
+                    stats["max_loss_turn"]["player"] = player
+                    stats["max_loss_turn"]["value"] = potential_turn_score
 
             else:
                 print(f"you win this turn, scoring {potential_turn_score} pts\n")
                 scoreboard[player]["score"] += potential_turn_score
-                scoring_turn[0] += 1
-                scoring_turn[1] += potential_turn_score
+                stats["scoring_turn"]["turns"] += 1
+                stats["scoring_turn"]["score"] += potential_turn_score
 
-                if potential_turn_score > scored_max_turn[1]:
-                    scored_max_turn[0] = player
-                    scored_max_turn[1] = potential_turn_score
+                if potential_turn_score > stats["scored_max_turn"]["value"]:
+                    stats["scored_max_turn"]["player"] = player
+                    stats["scored_max_turn"]["value"] = potential_turn_score
 
             
-            #ENDGAME
             if scoreboard[player]["score"] >= DEFAULT_TARGET_SCORE:
                 print(f"Game in {current_turn} turns")
                 sorted_scoreboard = get_sorted_scoreboard(scoreboard)
@@ -179,19 +221,11 @@ def game():
                     else:
                         print(f"{player} lose ! scoring {sorted_scoreboard[player]['score']} in {sorted_scoreboard[player]['rolls']} rolls with {sorted_scoreboard[player]['full_roll']} full roll, {sorted_scoreboard[player]['bonus']} bonus and {sorted_scoreboard[player]['max_potential_lost']} potential points lost")
                     
-                    
-                print("\n")
-                print(f"Max turn scoring : {scored_max_turn[0]} with {scored_max_turn[1]}")
-                print(f"Longest turn : {longest_turn[0]} with {longest_turn[1]}")
-                print(f"Max turn loss : {max_loss_turn[0]} with {max_loss_turn[1]}")
-
-                print("\n")
-                print(f"Mean scoring turn : {round(scoring_turn[1] / scoring_turn[0])} ({scoring_turn[0]} turns)")
-                print(f"Mean non scoring turn : {round(non_scoring_turn[1] / non_scoring_turn[0])} ({non_scoring_turn[0]} turns)")
+                
+                print_endgame_stats(stats)
                 is_finished = True
                 break
         
-
 
   
 game()
